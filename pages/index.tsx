efdeb8b92c9f39b25b24395a5a3fe3f5764fa5dc
springhana <xksxksanfro7@naver.com>
2023-05-09 22:24:32 +0900

    commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,6 +21,7 @@ export default function Home() {
   const [movie, setMovie] = useState<Movie[]>([]);
   const [mainPath, setMainPath] = useState<string[]>([]);
   const [mainTitle, setMainTitle] = useState<string[]>([]);
+  
   async function getDatas() {
     const response = await (await fetch(`/api/movies`)).json();
     setMovie(response.results);
